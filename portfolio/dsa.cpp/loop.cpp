1e95fe56f10095899e481f4d64bbaee356641a7b
Ayushi-choudhary22 <ayushichoudhary@2205>
2024-04-08 20:02:13 +0530

    Fixed readme

---
+++ portfolio/dsa.cpp/loop.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+int i,n;
+cout<<n;
+cin>>n;
+if(i%2==0)
+{
+int i=1;
+int sum=0;
+sum=sum+i;
+i=i+1;
+cout<<"sum of even numbers are= "<<sum<<endl;
+}
+else{
+    cout<<"numer is not even"<<endl;
+}
+
